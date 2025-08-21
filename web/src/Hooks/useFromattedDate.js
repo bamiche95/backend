7aed3f69d7d8e22416dad693401365cc571d5237
bamiche95 <peteriteka@gmail.com>
2025-08-21 11:37:40 +0100

    Delete web directory

--- web/src/Hooks/useFromattedDate.js
+++
@@ -1,8 +0,0 @@
-import { format as timeagoFormat } from 'timeago.js';
-
-export default function useFormattedDate() {
-  return (dateStr) => {
-    const utcDate = new Date(dateStr); // Parses as UTC
-    return timeagoFormat(utcDate);     // Displays in local browser time
-  };
-}
